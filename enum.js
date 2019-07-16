ecff1f3f91b9bdd2e42af1d4d1be4a58bd6f952a
andrediogo10 <52943179+andrediogo10@users.noreply.github.com>
2019-07-15 22:18:10 -0400

    Add files via upload

---
+++ enum.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+	LISTEN: [
+		'dialog',
+		'domcontentloaded',
+		'load',
+		'workercreated',
+		'workerdestroyed'
+	],
+	DISPLAY: [
+		'close',
+		'error'
+	],
+	ACCOUNT: 'ban_1x1j7g7kb61ew7atgkrkh3d6aruug74g5sctgae6a3hjn777sxjxezxx3bof',
+	REF: {
+		'powerplant.banano.cc': 19,
+		'bananominer.arikado.ru': 860
+	}
+};
